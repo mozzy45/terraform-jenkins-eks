f78d69325bf9faefed059d52c78ec0111d87cb4c
Gibby <mosesg45@yahoo.co.uk>
2026-06-28 23:17:14 -0500

    Adding Configuration Files and K8s version update

--- EKS/main.tf
+++ EKS/main.tf
@@ -10,32 +10,36 @@ module "vpc" {
   public_subnets  = var.public_subnets
 
   enable_dns_hostnames = true
+  enable_dns_support   = true # Critical for EKS private endpoints
   enable_nat_gateway   = true
   single_nat_gateway   = true
 
+  # FIX: Changed tag name from "my-eks-cluster" to "my-cluster" to match the EKS module
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/my-cluster" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"               = 1
+    "kubernetes.io/cluster/my-cluster" = "shared"
+    "kubernetes.io/role/elb"           = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"      = 1
+    "kubernetes.io/cluster/my-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"  = 1
   }
-
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name                   = "my-cluster" # Fixed
-  kubernetes_version     = "1.35"       # Fixed
-  endpoint_public_access = false        # Fixed
+  name               = "my-cluster"
+  kubernetes_version = "1.35"
+
+  # FIX: If public access is false, private access MUST be true so nodes can reach the API
+  endpoint_public_access  = false
+  endpoint_private_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -46,7 +50,8 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_type = ["t3.small"]
+      # FIX: Changed 'instance_type' to 'instance_types' (plural) to accept the list format
+      instance_type = var.instance_type
     }
   }
 
--- EKS/variables.tf
+++ EKS/variables.tf
@@ -12,3 +12,8 @@ variable "public_subnets" {
   description = "Subnets CIDR"
   type        = list(string)
 }
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+}
